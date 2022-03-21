8f6a98cfbc688a7933233b70e04bced177389790
Rick Rieger <rickrieger36251@gmail.com>
2022-03-21 12:01:41 -0400

    added a query to find winner based on length of powers between hero and villain

--- supers/views.py
+++ supers/views.py
@@ -12,21 +12,36 @@ from powers.serializers import PowersSerializer
 def supers_list(request):
 
   type = request.query_params.get('type')
-  
+  hero = request.query_params.get('hero')
+  villain = request.query_params.get('villain')
+
+
   if request.method == 'GET':
     supers = Super.objects.all() 
     if type:
       supers = supers.filter(super_type_id__type = type)                    
       serializer = SuperSerializer(supers, many=True)
-      return Response(serializer.data)
+      return Response(serializer.data, status=status.HTTP_200_OK)
+    elif hero and villain:
+        hero_found = get_object_or_404(Super,name=hero)
+        villain_found = get_object_or_404(Super,name=villain)
+        hero_powers = Power.objects.filter(super = hero_found.id)
+        villain_powers = Power.objects.filter(super = villain_found.id)
+        hero_found_serialized = SuperSerializer(hero_found)
+        villain_found_serialized = SuperSerializer(villain_found)
+        if len(hero_powers) > len(villain_powers):
+            return Response({'winner':hero_found_serialized.data, 'looser':villain_found_serialized.data}, status=status.HTTP_200_OK)
+        elif len(hero_powers) < len(villain_powers):
+            return Response({'winner':villain_found_serialized.data, 'looser':hero_found_serialized.data}, status=status.HTTP_200_OK)
+        elif len(hero_powers) == len(villain_powers):
+            return Response({'draw':hero_found_serialized.data, 'draw':villain_found_serialized.data}, status=status.HTTP_200_OK)
     else: 
       heros = supers.filter(super_type_id__type = 'hero')
       villains = supers.filter(super_type_id__type = 'villain')
-    
       heros = SuperSerializer(heros, many=True)
       villains = SuperSerializer(villains, many=True)
       result = {'heros':heros.data, 'villains':villains.data}
-      return Response(result)
+      return Response(result, status=status.HTTP_200_OK)
 
 
   elif request.method == 'POST':   
@@ -40,10 +55,9 @@ def supers_list(request):
 def supers_detail(request, pk): 
   super = get_object_or_404(Super, pk=pk)
   power = request.query_params.get('power')
- 
-  if request.method == 'GET': 
+  if request.method == 'GET':
     serializer = SuperSerializer(super)
-    return Response(serializer.data) 
+    return Response(serializer.data, status=status.HTTP_200_OK) 
   elif request.method == 'PUT':
     if power:
       new_power = Power(name=power)
